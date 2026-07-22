dd85ba62f76bf61a1a9154f5f66d10dedc10ff17
guspyy <gustavoffavero02@gmail.com>
2026-07-22 11:56:03 -0300

    versao aperfeicoada

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
         int opcao = 0;
 
+        int prosseguir = 0;
+
         float n1 = 0F;
         float n2 = 0F;
         float resultado = 0F;
@@ -21,38 +23,50 @@ public class Main {
             System.out.print("Digite o 2o número: ");
             n2 = scan.nextFloat();
 
-            System.out.printf("Que operação você deseja fazer\n" +
-                    "[0] - Sair\n" +
-                    "[1] - Soma\n" +
-                    "[2] - Subtrair\n" +
-                    "[3] - Multiplicação\n" +
-                    "[4] - Divisão\n" +
-                    "> ");
-            opcao = scan.nextInt();
-            switch (opcao){
-                case 0:
-                    System.out.println("Saindo...");
-                    System.exit(0);
-                    break;
-                case 1:
-                    resultado = SOMA.somar(n1, n2);
-                    operacao = '+';
-                    break;
-                case 2:
-                    resultado = SUBTRACAO.subtrair(n1, n2);
-                    operacao = '-';
-                    break;
-                case 3:
-                    resultado = MULTIPLICACAO.multiplicar(n1, n2);
-                    operacao = 'x';
-                    break;
-                case 4:
-                    resultado = DIVISAO.dividir(n1, n2);
-                    operacao = '/';
-                    break;
+            while (prosseguir == 0){
+                System.out.printf("Que operação você deseja fazer\n" +
+                        "[0] - Sair\n" +
+                        "[1] - Soma\n" +
+                        "[2] - Subtrair\n" +
+                        "[3] - Multiplicação\n" +
+                        "[4] - Divisão\n" +
+                        "> ");
+                opcao = scan.nextInt();
+
+                switch (opcao){
+                    case 0:
+                        System.out.println("Saindo...");
+                        System.exit(0);
+                        break;
+                    case 1:
+                        resultado = SOMA.somar(n1, n2);
+                        operacao = '+';
+                        prosseguir = 1;
+                        break;
+                    case 2:
+                        resultado = SUBTRACAO.subtrair(n1, n2);
+                        operacao = '-';
+                        prosseguir = 1;
+                        break;
+                    case 3:
+                        resultado = MULTIPLICACAO.multiplicar(n1, n2);
+                        operacao = 'x';
+                        prosseguir = 1;
+                        break;
+                    case 4:
+                        resultado = DIVISAO.dividir(n1, n2);
+                        operacao = '/';
+                        prosseguir = 1;
+                        if (n2 == 0) { prosseguir = 0; }
+                        break;
+                    default:
+                        prosseguir = 0;
+                        break;
+                }
             }
 
             System.out.printf("%.2f %c %.2f = %.2f\n", n1, operacao, n2, resultado);
+            prosseguir = 0; // Reseta variável para próx. loop
 
             try (FileWriter writer = new FileWriter("historico.txt", true)){
                 String linhaEscrita = String.format("-OPERACAO-\n" +
